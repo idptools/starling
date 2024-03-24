6f888a4f2799f996f005f2def7ad1863eca387f4
bnovakborna <bnovak@wustl.edu>
2024-03-24 12:25:45 -0500

    working on vae-predict script

---
+++ starling/training/vae_predict.py
@@ -0,0 +1,88 @@
+import argparse
+
+import numpy as np
+import pytorch_lightning as pl
+import torch
+from IPython import embed
+
+from starling.models.vae import VAE
+from starling.training.myloader import MatrixDataModule
+
+
+def vae_predict():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default=None,
+        help="Path to the checkpoint to load in",
+    )
+
+    parser.add_argument(
+        "--test_data",
+        type=str,
+        default=None,
+        help="Data you want to test your model on",
+    )
+
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=16,
+        help="Batch size for training",
+    )
+
+    parser.add_argument(
+        "--loss_type",
+        type=str,
+        default="weighted_mse",
+        help="""What loss to calculate for the reconstruction loss, current losses include 
+        mse and weighted_mse""",
+    )
+
+    parser.add_argument(
+        "--normalize",
+        type=str,
+        default=None,
+        help="""Whether to normalize the distance map. Options:[length, afrc, bond_length].
+        Length is normalizing by np.sqrt(length), afrc by AFRC distance map, and bond_length 
+        by the accumulated count of residues for each element within a distance map*bond_length""",
+    )
+
+    parser.add_argument(
+        "--interpolate",
+        action="store_true",
+        help="Enable interpolation of distance maps through torch resizing using bicubic method",
+    )
+
+    parser.add_argument(
+        "--gpu_id",
+        type=int,
+        default=0,
+        help="GPU device ID for training",
+    )
+
+    args = parser.parse_args()
+
+    dataset = MatrixDataModule(
+        test_data=args.test_data, args=args, batch_size=args.batch_size
+    )
+
+    dataset.setup(stage="predict")
+    predict_dataloader = dataset.predict_dataloader()
+
+    device = torch.device(f"cuda:{args.gpu_id}")
+    model = VAE.load_from_checkpoint(args.model_path, map_location=device)
+    model.eval()
+
+    for batch in predict_dataloader:
+        x = batch["input"].to(f"cuda:{args.gpu_id}")
+        x_reconstructed = model(x)[0]
+        np.save("ground_truth.npy", x.cpu().detach().numpy())
+        np.save("reconstructed_array.npy", x_reconstructed.cpu().detach().numpy())
+        break
+
+
+if __name__ == "__main__":
+    vae_predict()
