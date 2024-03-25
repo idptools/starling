33b1264d7784137326e9a7896e4e60598e645539
bnovakborna <bnovak@wustl.edu>
2024-03-25 10:41:17 -0500

    working on vae-predict script 2

--- starling/training/vae_predict.py
+++ starling/training/vae_predict.py
@@ -59,29 +59,54 @@ def vae_predict():
     parser.add_argument(
         "--gpu_id",
         type=int,
-        default=0,
+        nargs="+",
+        default=[0],
         help="GPU device ID for training",
     )
 
     args = parser.parse_args()
 
     dataset = MatrixDataModule(
-        test_data=args.test_data, args=args, batch_size=args.batch_size
+        train_data=args.test_data,
+        val_data=args.test_data,
+        test_data=args.test_data,
+        predict_data=args.test_data,
+        args=args,
+        batch_size=args.batch_size,
     )
 
-    dataset.setup(stage="predict")
-    predict_dataloader = dataset.predict_dataloader()
+    # dataset.setup()
+    # predict_dataloader = dataset.predict_dataset()
 
-    device = torch.device(f"cuda:{args.gpu_id}")
+    # device = torch.device(f"cuda:{args.gpu_id}")
+    device = torch.device(f"cuda:{args.gpu_id[0]}")
     model = VAE.load_from_checkpoint(args.model_path, map_location=device)
     model.eval()
 
-    for batch in predict_dataloader:
-        x = batch["input"].to(f"cuda:{args.gpu_id}")
-        x_reconstructed = model(x)[0]
-        np.save("ground_truth.npy", x.cpu().detach().numpy())
-        np.save("reconstructed_array.npy", x_reconstructed.cpu().detach().numpy())
-        break
+    dataset.setup()
+    predict_dataloader = dataset.predict_dataloader()
+
+    embed()
+
+    # for batch in predict_dataloader:
+    #     x = batch["input"].to(f"cuda:{args.gpu_id[0]}")
+    #     x_reconstructed = model(x)[0]
+    #     np.save("ground_truth.npy", x.cpu().detach().numpy())
+    #     np.save("reconstructed_array.npy", x_reconstructed.cpu().detach().numpy())
+    #     break
+
+    # trainer = pl.Trainer(
+    #     devices=args.gpu_id,
+    # )
+    # embed()
+    # predictions = trainer.predict(model, dataloaders=dataset)
+
+    # for batch in dataset:
+    #     x = batch["input"].to(f"cuda:{args.gpu_id}")
+    #     x_reconstructed = model(x)[0]
+    #     np.save("ground_truth.npy", x.cpu().detach().numpy())
+    #     np.save("reconstructed_array.npy", x_reconstructed.cpu().detach().numpy())
+    #     break
 
 
 if __name__ == "__main__":
