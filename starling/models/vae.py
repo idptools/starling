cbf64e699f8dbb17b7bfa6b9aaf1805b2520ff20
bnovakborna <bnovak@wustl.edu>
2024-03-27 20:35:12 -0500

    vae works with config.yaml file

--- starling/models/vae.py
+++ starling/models/vae.py
@@ -22,16 +22,28 @@ torch.set_float32_matmul_precision("high")
 class VAE(pl.LightningModule):
     def __init__(
         self,
-        in_channels: int,
-        latent_dim: int,
-        deep: int,
-        kernel_size: int,
-        loss_type: str,
+        in_channels,
+        latent_dim,
+        num_layers,
+        kernel_size,
+        starting_hidden_dim,
+        dimension,
+        loss_type,
+        weights_type,
+        KLD_weight,
+        lr_scheduler,
     ):
         super().__init__()
 
         self.save_hyperparameters()
+
+        # Loss params
         self.loss_type = loss_type
+        self.weights_type = weights_type
+        self.config_scheduler = lr_scheduler
+
+        # KLD loss params
+        self.KLD_weight = KLD_weight
 
         # these are used to monitor the training losses for the *EPOCH*
         self.total_train_step_losses = []
@@ -40,16 +52,9 @@ class VAE(pl.LightningModule):
 
         self.monitor = "epoch_val_loss"
 
-        # Hard coded, should we actually start at 8 or 16
-        starting_hidden_dim = 32
+        self.hidden_dims = [starting_hidden_dim * 2**i for i in range(num_layers)]
 
-        self.hidden_dims = [starting_hidden_dim * 2**i for i in range(deep)]
-        # This is hard coded in
-        size_of_distance_map = 192
-        # size_of_distance_map = 384
-        # size_of_distance_map = 768
-
-        shape = int(size_of_distance_map / 2 ** len(self.hidden_dims))
+        shape = int(dimension / 2 ** len(self.hidden_dims))
         modules = []
 
         for num, hidden_dim in enumerate(self.hidden_dims):
@@ -66,8 +71,8 @@ class VAE(pl.LightningModule):
                     nn.LayerNorm(
                         [
                             hidden_dim,
-                            int(size_of_distance_map / (2 ** (num + 1))),
-                            int(size_of_distance_map / (2 ** (num + 1))),
+                            int(dimension / (2 ** (num + 1))),
+                            int(dimension / (2 ** (num + 1))),
                         ]
                     ),
                     # nn.GroupNorm(int(hidden_dim / 4), int(hidden_dim)),
@@ -137,8 +142,8 @@ class VAE(pl.LightningModule):
             nn.LayerNorm(
                 [
                     reverse_hidden_dims[-1],
-                    size_of_distance_map,
-                    size_of_distance_map,
+                    dimension,
+                    dimension,
                 ]
             ),
             # nn.InstanceNorm2d(reverse_hidden_dims[-1]),
@@ -156,12 +161,10 @@ class VAE(pl.LightningModule):
         )
 
         if self.loss_type == "elbo":
-            # self.log_scale = nn.Parameter(
-            #     torch.zeros(size_of_distance_map, size_of_distance_map)
+            # self.log_std = nn.Parameter(
+            #     torch.zeros(dimension, dimension)
             # )
-            self.log_scale = nn.Parameter(
-                torch.zeros((size_of_distance_map * (size_of_distance_map + 1) // 2))
-            )
+            self.log_std = nn.Parameter(torch.zeros((dimension * (dimension + 1) // 2)))
 
     def encode(self, x: torch.Tensor):
         z = self.encoder(x)
@@ -184,7 +187,7 @@ class VAE(pl.LightningModule):
         eps = torch.randn_like(std)
         return mu + eps * std
 
-    def get_weights(self, ground_truth, scale="reciprocal"):
+    def get_weights(self, ground_truth, scale):
         if scale == "linear":
             max_distance = ground_truth.max()
             min_distance = ground_truth.min()
@@ -204,103 +207,71 @@ class VAE(pl.LightningModule):
         symmetrized_array = upper_triangle + upper_triangle.t()
         return symmetrized_array.fill_diagonal_(0)
 
-    def linear_KLD_beta(self, start, stop, n_epoch, n_cycle=5, ratio=1):
-        # Compute the linear schedule for KLD weighting in the loss function
-        L = torch.ones(n_epoch).to(self.device)
-        period = n_epoch / n_cycle  # Compute period length with floating-point division
-        step = (stop - start) / (period * ratio)  # Linear schedule step size
-
-        for c in range(n_cycle):
-            v, i = start, 0
-            while v <= stop and int(i + c * period) < n_epoch:
-                L[int(i + c * period)] = v
-                v += step
-                i += 1
-
-        return L
-
-    def gaussian_likelihood(self, x_hat, logscale, x):
-        scale = torch.exp(logscale)
-        mean = x_hat
+    def gaussian_likelihood(self, data_hat, log_std, data):
+        std = torch.exp(log_std)
+        mean = data_hat
         input_size = mean.shape[0]
-        matrix_scale = torch.zeros(input_size, input_size).to(self.device)
+        matrix_std = torch.zeros(input_size, input_size).to(self.device)
         triu_indices = torch.triu_indices(input_size, input_size, offset=0)
-        matrix_scale[triu_indices[0], triu_indices[1]] = scale[
+        matrix_std[triu_indices[0], triu_indices[1]] = std[
             : input_size * (input_size + 1) // 2
         ]
-        matrix_scale = matrix_scale + matrix_scale.t() - torch.diag(matrix_scale.diag())
-        dist = torch.distributions.Normal(mean, matrix_scale)
+        matrix_std = matrix_std + matrix_std.t() - torch.diag(matrix_std.diag())
+        dist = torch.distributions.Normal(mean, matrix_std)
         # measure prob of seeing image under p(x|z)
-        log_pxz = dist.log_prob(x)
-        weights = self.get_weights(x, scale="reciprocal")
+        log_pxz = dist.log_prob(data)
+        weights = self.get_weights(data, scale=self.weights_type)
         log_pxz *= weights
         return log_pxz.sum()
 
-    def kl_divergence(self, z, mu, std):
-        # --------------------------
-        # Monte carlo KL divergence
-        # --------------------------
-        # 1. define the first two probabilities (in this case Normal for both)
-        p = torch.distributions.Normal(torch.zeros_like(mu), torch.ones_like(std))
-        q = torch.distributions.Normal(mu, std)
-
-        # 2. get the probabilities from the equation
-        log_qzx = q.log_prob(z)
-        log_pz = p.log_prob(z)
-
-        # kl
-        kl = log_qzx - log_pz
-        kl = kl.sum(-1)
-        return kl
-
     def vae_loss(
         self,
-        x_reconstructed,
-        x,
-        latent_encoding,
+        data_reconstructed,
+        data,
         mu,
         logvar,
-        loss_type: str,
-        scale="reciprocal",
-        beta=1,
     ):
         # Find where the padding starts by counting the number of
-        start_of_padding = torch.sum(x != 0, dim=(1, 2))[:, 0] + 1
+        start_of_padding = torch.sum(data != 0, dim=(1, 2))[:, 0] + 1
 
-        if loss_type == "mse" or loss_type == "weighted_mse":
+        if self.loss_type == "mse" or self.loss_type == "weighted_mse":
             # Loss function for VAE, here I am removing the padded region
             # from both the ground truth and prediction
 
             BCE = 0
 
             for num, padding_start in enumerate(start_of_padding):
-                x_reconstructed_no_padding = x_reconstructed[num][0][
+                data_reconstructed_no_padding = data_reconstructed[num][0][
                     :padding_start, :padding_start
                 ]
                 # Make the reconstructed map symmetric so that weights are freed to learn other
                 # patterns
-                x_reconstructed_no_padding = self.symmetrize(x_reconstructed_no_padding)
+                data_reconstructed_no_padding = self.symmetrize(
+                    data_reconstructed_no_padding
+                )
 
                 # Get unpadded ground truth
-                x_no_padding = x[num][0][:padding_start, :padding_start]
+                data_no_padding = data[num][0][:padding_start, :padding_start]
 
                 # Mean squared error weighted by ground truth distance
-                if loss_type == "weighted_mse":
-                    weights = self.get_weights(x_no_padding, scale=scale)
+                if self.loss_type == "weighted_mse":
+                    weights = self.get_weights(data_no_padding, scale=self.weights_type)
 
                     mse_loss = F.mse_loss(
-                        x_reconstructed_no_padding, x_no_padding, reduction="none"
+                        data_reconstructed_no_padding, data_no_padding, reduction="none"
                     )
 
                     BCE += (mse_loss * weights).sum()
 
                 # Mean squared error not weighted by ground truth distance
-                elif loss_type == "mse":
+                elif self.loss_type == "mse":
                     BCE += F.mse_loss(
-                        x_reconstructed_no_padding, x_no_padding, reduction="mean"
+                        data_reconstructed_no_padding, data_no_padding, reduction="mean"
                     )
                 else:
-                    raise ValueError(f"loss type of name '{loss_type}' does not exist")
+                    raise ValueError(
+                        f"loss type of name '{self.loss_type}' does not exist"
+                    )
 
             # Taking the mean of the loss (could also be sum)
             BCE /= num + 1
@@ -312,79 +283,59 @@ class VAE(pl.LightningModule):
             KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1)
             KLD = torch.logsumexp(KLD, dim=0) / mu.size(0)  # Mean over batch
 
-            loss = BCE + beta * KLD
+            loss = BCE + self.KLD_weight * KLD
 
             return {"loss": loss, "BCE": BCE, "KLD": KLD}
 
-        elif loss_type == "elbo":
+        elif self.loss_type == "elbo":
             total_recon_loss = 0
             for num, padding_start in enumerate(start_of_padding):
-                x_reconstructed_no_padding = x_reconstructed[num][0][
+                data_reconstructed_no_padding = data_reconstructed[num][0][
                     :padding_start, :padding_start
                 ]
                 # Make the reconstructed map symmetric so that weights are
                 # freed to learn other patterns
-                x_reconstructed_no_padding = self.symmetrize(x_reconstructed_no_padding)
+                data_reconstructed_no_padding = self.symmetrize(
+                    data_reconstructed_no_padding
+                )
 
                 # Get unpadded ground truth
-                x_no_padding = x[num][0][:padding_start, :padding_start]
+                data_no_padding = data[num][0][:padding_start, :padding_start]
 
                 # Get the reconstruction loss
                 recon_loss = self.gaussian_likelihood(
-                    x_reconstructed_no_padding, self.log_scale, x_no_padding
+                    data_hat=data_reconstructed_no_padding,
+                    log_std=self.log_std,
+                    data=data_no_padding,
                 )
                 total_recon_loss += recon_loss
 
             # Take the mean of all the losses in batch
             total_recon_loss /= num + 1
 
-            # std = torch.exp(logvar / 2)
-            # KLD = self.kl_divergence(latent_encoding, mu, std)
             KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1)
             KLD = torch.logsumexp(KLD, dim=0) / mu.size(0)  # Mean over batch
-            elbo = beta * KLD - total_recon_loss
-            # elbo = KLD.mean() - total_recon_loss
+            elbo = self.KLD_weight * KLD - total_recon_loss
 
             return {"loss": elbo, "BCE": total_recon_loss, "KLD": KLD}
 
-    def forward(self, x):
-        mu, logvar = self.encode(x)
+    def forward(self, data):
+        mu, logvar = self.encode(data)
+        latent_encoding = self.reparameterize(mu, logvar)
 
-        if self.loss_type in ["mse", "weighted_mse"]:
-            latent_encoding = self.reparameterize(mu, logvar)
-
-        elif self.loss_type == "elbo":
-            # std = torch.exp(logvar / 2)
-            # q = torch.distributions.Normal(mu, std)
-            # latent_encoding = q.rsample()
-            latent_encoding = self.reparameterize(mu, logvar)
-
-        else:
-            raise ValueError(f"loss type of name {self.loss_type} not implemented")
-
-        x_reconstructed = self.decode(latent_encoding)
-
-        return x_reconstructed, mu, logvar, latent_encoding
+        data_reconstructed = self.decode(latent_encoding)
+        return data_reconstructed, mu, logvar, latent_encoding
 
     def training_step(self, batch, batch_idx):
-        x = batch["input"]
+        data = batch["input"]
 
-        # Calculate the schedule for linear scaling of beta for KLD loss
-        # Only calculate if we just started to train
-        # if self.trainer.global_step == 0:
-        #     self.KLD_betas = self.linear_KLD_beta(
-        #         start=0, stop=5, n_epoch=self.trainer.max_epochs, ratio=1
-        #     )
-
-        x_reconstructed, mu, logvar, latent_encoding = self.forward(x)
+        data_reconstructed, mu, logvar, latent_encoding = self.forward(data=data)
 
         loss = self.vae_loss(
-            x_reconstructed=x_reconstructed,
-            x=x,
-            latent_encoding=latent_encoding,
+            data_reconstructed=data_reconstructed,
+            data=data,
             mu=mu,
             logvar=logvar,
-            loss_type=self.loss_type,
         )
 
         self.total_train_step_losses.append(loss["loss"])
@@ -412,21 +363,15 @@ class VAE(pl.LightningModule):
         self.KLD_step_losses.clear()
 
     def validation_step(self, batch, batch_idx):
-        x = batch["input"]
+        data = batch["input"]
 
-        x_reconstructed, mu, logvar, latent_encoding = self.forward(x)
+        data_reconstructed, mu, logvar, latent_encoding = self.forward(data)
 
-        # What loss do we want to use in validation step
-        # A regular sum I think makes sense, instead of an
-        # ever changing beta
         loss = self.vae_loss(
-            x_reconstructed=x_reconstructed,
-            x=x,
-            latent_encoding=latent_encoding,
+            data_reconstructed=data_reconstructed,
+            data=data,
             mu=mu,
             logvar=logvar,
-            loss_type=self.loss_type,
-            beta=1,
         )
 
         self.log("epoch_val_loss", loss["loss"], prog_bar=True, sync_dist=True)
@@ -438,20 +383,26 @@ class VAE(pl.LightningModule):
             self.parameters(), lr=0.05, momentum=0.99, nesterov=True
         )
 
-        lr_scheduler = {
-            "scheduler": CosineAnnealingWarmRestarts(optimizer, T_0=5, eta_min=1e-4),
-            "monitor": self.monitor,
-            "interval": "epoch",
-        }
+        if self.config_scheduler == "CosineAnnealingWarmRestarts":
+            lr_scheduler = {
+                "scheduler": CosineAnnealingWarmRestarts(
+                    optimizer, T_0=5, eta_min=1e-4
+                ),
+                "monitor": self.monitor,
+                "interval": "epoch",
+            }
 
-        # lr_scheduler = {
-        #     "scheduler": OneCycleLR(
-        #         optimizer,
-        #         max_lr=0.01,
-        #         total_steps=self.trainer.estimated_stepping_batches,
-        #     ),
-        #     "monitor": self.monitor,
-        #     "interval": "step",
-        # }
+        elif self.config_scheduler == "OneCycleLR":
+            lr_scheduler = {
+                "scheduler": OneCycleLR(
+                    optimizer,
+                    max_lr=0.01,
+                    total_steps=self.trainer.estimated_stepping_batches,
+                ),
+                "monitor": self.monitor,
+                "interval": "step",
+            }
+        else:
+            raise ValueError(f"{self.config_scheduler} lr_scheduler is not implemented")
 
         return [optimizer], [lr_scheduler]
