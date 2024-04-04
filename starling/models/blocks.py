c72f9e21db9268484d5280d009869f0024656c2c
bnovakborna <bnovak@wustl.edu>
2024-04-04 15:24:59 -0500

    added layernorm to resnets

--- starling/models/blocks.py
+++ starling/models/blocks.py
@@ -26,73 +26,94 @@ class MinPool2d(nn.Module):
 
 class ResizeConv2d(nn.Module):
     def __init__(
-        self, in_channels, out_channels, kernel_size, scale_factor=2, mode="bilinear"
+        self, in_channels, out_channels, kernel_size, size=(12, 12), mode="nearest"
     ):
         super().__init__()
-        self.scale_factor = scale_factor
+        self.size = size
         self.mode = mode
-        self.conv = nn.Conv2d(
-            in_channels, out_channels, kernel_size, stride=1, padding=1
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride=1, padding=1),
+            # nn.BatchNorm2d(out_channels),
+            nn.LayerNorm([out_channels, self.size[0], self.size[1]]),
+            nn.ReLU(),
         )
 
     def forward(self, x):
-        x = F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+        x = F.interpolate(x, size=self.size, mode=self.mode)
         x = self.conv(x)
         return x
 
 
-class DownsampleBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
+class ResBlockEncBasic(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, dimension) -> None:
         super().__init__()
 
         padding = 2 if kernel_size == 5 else (3 if kernel_size == 7 else 1)
 
-        self.conv = nn.Sequential(
+        # First convolution of the ResNet with or without downsampling
+        # depending on the downsample flag (stride=1 or 2)
+        # (b, c, h, w) -> (b, c, h, w) stride = 1
+        # (b, c, h, w) -> (b, c*2, h /2, w /2 ) stride = 2
+        self.conv1 = nn.Sequential(
             nn.Conv2d(
                 in_channels=in_channels,
                 out_channels=out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
+                stride=2 if in_channels != out_channels else 1,
                 padding=padding,
+                kernel_size=kernel_size,
             ),
-            nn.BatchNorm2d(out_channels),
+            # nn.BatchNorm2d(out_channels),
+            layer_norm(out_channels, dimension),
             nn.ReLU(),
         )
 
-    def forward(self, data):
-        return self.conv(data)
-
-
-class UpsampleBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride):
-        super().__init__()
-
-        padding = 2 if kernel_size == 5 else (3 if kernel_size == 7 else 1)
-
-        self.conv_transpose = nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels,
-                out_channels,
-                kernel_size=kernel_size,
-                stride=stride,
+        # Second convolution which doesn't do any downsampling, but needs
+        # to setup in_channels and out_channels according to self.conv1
+        # (b, c, h, w) -> (b, c, h, w) stride = 1
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=out_channels,
+                out_channels=out_channels,
+                stride=1,
                 padding=padding,
-                output_padding=1,
+                kernel_size=kernel_size,
             ),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
+            # nn.BatchNorm2d(out_channels),
+            layer_norm(out_channels, dimension),
         )
 
+        # Set up the shortcut if downsampling is done
+        # (b, c, h, w) -> (b, c*2, h /2, w /2 ) stride = 2
+        if in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=1,
+                    stride=2,
+                    padding=0,
+                ),
+                # nn.BatchNorm2d(out_channels),
+                layer_norm(out_channels, dimension),
+            )
+        else:
+            self.shortcut = nn.Sequential()
+        self.activation = nn.ReLU()
+
     def forward(self, data):
-        return self.conv_transpose(data)
+        # Set up the shortcut connection if necessary
+        identity = self.shortcut(data)
+        # First convolution
+        out = self.conv1(data)
+        # Second convolution
+        out = self.conv2(out)
+        # Add the input and run it through activation function
+        out += identity
+        return self.activation(out)
 
 
 class ResBlockDecBasic(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        out_channels,
-        kernel_size,
-    ) -> None:
+    def __init__(self, in_channels, out_channels, kernel_size, dimension) -> None:
         super().__init__()
 
         padding = 2 if kernel_size == 5 else (3 if kernel_size == 7 else 1)
@@ -107,7 +128,8 @@ class ResBlockDecBasic(nn.Module):
                 padding=padding,
                 kernel_size=kernel_size,
             ),
-            nn.BatchNorm2d(in_channels),
+            # nn.BatchNorm2d(in_channels),
+            layer_norm(in_channels, dimension),
             nn.ReLU(),
         )
 
@@ -125,7 +147,8 @@ class ResBlockDecBasic(nn.Module):
                     padding=padding,
                     output_padding=1,
                 ),
-                nn.BatchNorm2d(out_channels),
+                # nn.BatchNorm2d(out_channels),
+                layer_norm(out_channels, dimension),
             )
             # Setup a shortcut connection
             self.shortcut = nn.Sequential(
@@ -137,7 +160,8 @@ class ResBlockDecBasic(nn.Module):
                     padding=0,
                     output_padding=1,
                 ),
-                nn.BatchNorm2d(out_channels),
+                # nn.BatchNorm2d(out_channels),
+                layer_norm(out_channels, dimension),
             )
         else:
             self.conv2 = nn.Sequential(
@@ -148,7 +172,8 @@ class ResBlockDecBasic(nn.Module):
                     padding=padding,
                     kernel_size=kernel_size,
                 ),
-                nn.BatchNorm2d(out_channels),
+                # nn.BatchNorm2d(out_channels),
+                layer_norm(out_channels, dimension),
             )
             self.shortcut = nn.Sequential()
 
@@ -168,73 +193,14 @@ class ResBlockDecBasic(nn.Module):
         return self.activation(out)
 
 
-class ResBlockEncBasic(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size) -> None:
-        super().__init__()
-
-        padding = 2 if kernel_size == 5 else (3 if kernel_size == 7 else 1)
-
-        # First convolution of the ResNet with or without downsampling
-        # depending on the downsample flag (stride=1 or 2)
-        # (b, c, h, w) -> (b, c, h, w) stride = 1
-        # (b, c, h, w) -> (b, c*2, h /2, w /2 ) stride = 2
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=out_channels,
-                stride=2 if in_channels != out_channels else 1,
-                padding=padding,
-                kernel_size=kernel_size,
-            ),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-        )
-
-        # Second convolution which doesn't do any downsampling, but needs
-        # to setup in_channels and out_channels according to self.conv1
-        # (b, c, h, w) -> (b, c, h, w) stride = 1
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels=out_channels,
-                out_channels=out_channels,
-                stride=1,
-                padding=padding,
-                kernel_size=kernel_size,
-            ),
-            nn.BatchNorm2d(out_channels),
-        )
-
-        # Set up the shortcut if downsampling is done
-        # (b, c, h, w) -> (b, c*2, h /2, w /2 ) stride = 2
-        if in_channels != out_channels:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(
-                    in_channels=in_channels,
-                    out_channels=out_channels,
-                    kernel_size=1,
-                    stride=2,
-                    padding=0,
-                ),
-                nn.BatchNorm2d(out_channels),
-            )
-        else:
-            self.shortcut = nn.Sequential()
-        self.activation = nn.ReLU()
-
-    def forward(self, data):
-        # Set up the shortcut connection if necessary
-        identity = self.shortcut(data)
-        # First convolution
-        out = self.conv1(data)
-        # Second convolution
-        out = self.conv2(out)
-        # Add the input and run it through activation function
-        out += identity
-        return self.activation(out)
-
-
 def instance_norm(features, eps=1e-6, **kwargs):
     return nn.InstanceNorm2d(features, affine=True, eps=eps, **kwargs)
+
+
+def layer_norm(out_channels, starting_dimension, **kwargs):
+    denominator = 4 * (out_channels / 64)
+    dimension = int(starting_dimension / denominator)
+    return nn.LayerNorm([out_channels, dimension, dimension])
 
 
 class vanilla_Encoder(nn.Module):
@@ -337,3 +303,48 @@ class vanilla_Decoder(nn.Module):
 
     def forward(self, data):
         return self.decoder(data)
+
+
+class DownsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super().__init__()
+
+        padding = 2 if kernel_size == 5 else (3 if kernel_size == 7 else 1)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, data):
+        return self.conv(data)
+
+
+class UpsampleBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super().__init__()
+
+        padding = 2 if kernel_size == 5 else (3 if kernel_size == 7 else 1)
+
+        self.conv_transpose = nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels,
+                out_channels,
+                kernel_size=kernel_size,
+                stride=stride,
+                padding=padding,
+                output_padding=1,
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
+    def forward(self, data):
+        return self.conv_transpose(data)
