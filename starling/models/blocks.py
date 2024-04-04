1475a02cc1b44758556059ce80643e02b3898cf1
bnovakborna <bnovak@wustl.edu>
2024-04-03 23:25:50 -0500

    moved encoder decoder out of the blocks and changed the name of blocks

--- starling/models/blocks.py
+++ starling/models/blocks.py
@@ -3,6 +3,27 @@ from IPython import embed
 from torch import nn
 
 
+class MinPool2d(nn.Module):
+    def __init__(self, kernel_size, stride=None, padding=0, dilation=1):
+        super(MinPool2d, self).__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride or kernel_size
+        self.padding = padding
+        self.dilation = dilation
+
+    def forward(self, x):
+        # Perform min pooling using torch.min and torch.nn.functional.max_pool2d
+        unpool = nn.functional.max_pool2d(
+            -1 * x,
+            kernel_size=self.kernel_size,
+            stride=self.stride,
+            padding=self.padding,
+            dilation=self.dilation,
+            return_indices=False,
+        )
+        return -1 * unpool
+
+
 class ResizeConv2d(nn.Module):
     def __init__(
         self, in_channels, out_channels, kernel_size, scale_factor=2, mode="bilinear"
@@ -65,7 +86,7 @@ class UpsampleBlock(nn.Module):
         return self.conv_transpose(data)
 
 
-class ResBlockDec(nn.Module):
+class ResBlockDecBasic(nn.Module):
     def __init__(
         self,
         in_channels,
@@ -147,7 +168,7 @@ class ResBlockDec(nn.Module):
         return self.activation(out)
 
 
-class ResBlockEnc(nn.Module):
+class ResBlockEncBasic(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size) -> None:
         super().__init__()
 
@@ -316,136 +337,3 @@ class vanilla_Decoder(nn.Module):
 
     def forward(self, data):
         return self.decoder(data)
-
-
-class ResNet_Encoder(nn.Module):
-    def __init__(
-        self,
-        in_channels,
-        num_blocks,
-        kernel_size,
-    ) -> None:
-        super().__init__()
-
-        # First convolution of the ResNet Encoder
-        # Reduction in the spatial dimensions / 2
-        # with kernel=7 and stride=2
-        # maxpool reduces spatial dimensions by / 2
-        self.first_conv = nn.Sequential(
-            nn.Conv2d(
-                in_channels=in_channels,
-                out_channels=64,
-                kernel_size=7,
-                stride=2,
-                padding=3,
-            ),
-            nn.BatchNorm2d(64),
-            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-        )
-
-        self.in_channels = 64
-
-        # self.layers = []
-
-        # for num, block in enumerate(num_blocks):
-        #     if num == 0:
-        #         self.layers.append(
-        #             self._make_layer(ResBlockEnc, self.in_channels, block, kernel_size)
-        #         )
-        #     else:
-        #         self.layers.append(
-        #             self._make_layer(
-        #                 ResBlockEnc, self.in_channels * 2, num_blocks[0], kernel_size
-        #             )
-        #         )
-
-        self.layer1 = self._make_layer(ResBlockEnc, 64, num_blocks[0], kernel_size)
-        self.layer2 = self._make_layer(ResBlockEnc, 128, num_blocks[1], kernel_size)
-        self.layer3 = self._make_layer(ResBlockEnc, 256, num_blocks[2], kernel_size)
-        self.layer4 = self._make_layer(ResBlockEnc, 512, num_blocks[3], kernel_size)
-
-        self.average_pool = nn.AdaptiveAvgPool2d(1)
-
-    def _make_layer(self, residual_block, out_channels, num_blocks, kernel_size):
-        layers = []
-        for block in range(num_blocks):
-            layers += [residual_block(self.in_channels, out_channels, kernel_size)]
-            self.in_channels = out_channels
-        return nn.Sequential(*layers)
-
-    def forward(self, data):
-        data = self.first_conv(data)
-        # for layer in self.layers:
-        #     data = layer(data)
-        data = self.layer1(data)
-        data = self.layer2(data)
-        data = self.layer3(data)
-        data = self.layer4(data)
-        # The final adaptive average can also be done through convolution
-        # data = F.adaptive_avg_pool2d(data, 1)
-        data = self.average_pool(data)
-        return data
-
-
-class ResNet_Decoder(nn.Module):
-    def __init__(
-        self,
-        out_channels,
-        num_blocks,
-        kernel_size,
-    ) -> None:
-        super().__init__()
-
-        self.in_channels = 512
-
-        self.layer4 = self._make_layer(ResBlockDec, 256, num_blocks[0], kernel_size)
-        self.layer3 = self._make_layer(ResBlockDec, 128, num_blocks[1], kernel_size)
-        self.layer2 = self._make_layer(ResBlockDec, 64, num_blocks[2], kernel_size)
-        self.layer1 = self._make_layer(ResBlockDec, 64, num_blocks[3], kernel_size)
-
-        # This part could be done through interpolation (analogous to MaxPool)
-        self.reshaping_conv = nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels=64,
-                out_channels=64,
-                kernel_size=kernel_size,
-                stride=2,
-                padding=1,
-                output_padding=1,
-            ),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-        )
-
-        # Final output layer that looks similar to the first layer of
-        # the ResNet Encoder
-        self.output_layer = nn.Sequential(
-            nn.ConvTranspose2d(
-                in_channels=64,
-                out_channels=out_channels,
-                kernel_size=7,
-                stride=2,
-                padding=3,
-                output_padding=1,
-            ),
-            nn.ReLU(),
-        )
-
-    def _make_layer(self, residual_block, out_channels, num_blocks, kernel_size):
-        layers = []
-        for block in range(num_blocks - 1):
-            layers += [residual_block(self.in_channels, self.in_channels, kernel_size)]
-        layers += [residual_block(self.in_channels, out_channels, kernel_size)]
-        self.in_channels = out_channels
-        return nn.Sequential(*layers)
-
-    def forward(self, data):
-        # We could maybe do this with a linear layer instead
-        data = F.interpolate(data, size=(12, 12))
-        data = self.layer4(data)
-        data = self.layer3(data)
-        data = self.layer2(data)
-        data = self.layer1(data)
-        data = self.reshaping_conv(data)
-        data = self.output_layer(data)
-        return data
