b7abf604865e26b16ea53758175058b7075dc22e
bnovakborna <bnovak@wustl.edu>
2024-04-22 14:29:52 -0500

    added layernorm to resnets

--- starling/models/blocks.py
+++ starling/models/blocks.py
@@ -4,6 +4,40 @@ from IPython import embed
 from torch import nn
 
 
+class LayerNorm(nn.Module):
+    r"""LayerNorm that supports two data formats: channels_last (default) or channels_first.
+    The ordering of the dimensions in the inputs. channels_last corresponds to inputs with
+    shape (batch_size, height, width, channels) while channels_first corresponds to inputs
+    with shape (batch_size, channels, height, width).
+
+    Modified from:
+    https://github.com/facebookresearch/ConvNeXt/blob/main/models/convnext.py#L119
+    It increases memory requirements substantially, unclear if that can be changed
+    """
+
+    def __init__(self, normalized_shape, eps=1e-6, data_format="channels_first"):
+        super().__init__()
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        self.eps = eps
+        self.data_format = data_format
+        if self.data_format not in ["channels_last", "channels_first"]:
+            raise NotImplementedError
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x):
+        if self.data_format == "channels_last":
+            return F.layer_norm(
+                x, self.normalized_shape, self.weight, self.bias, self.eps
+            )
+        elif self.data_format == "channels_first":
+            x = x.permute(0, 2, 3, 1)
+            x = F.layer_norm(x, self.normalized_shape, self.weight, self.bias, self.eps)
+            x = x.permute(0, 3, 1, 2)
+
+            return x
+
+
 class MinPool2d(nn.Module):
     def __init__(self, kernel_size, stride=None, padding=0, dilation=1):
         super(MinPool2d, self).__init__()
