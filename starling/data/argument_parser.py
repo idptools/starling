535a0b09e50ca73219636b10ae3f9fac413e7c00
bnovakborna <bnovak@wustl.edu>
2024-05-01 16:05:15 -0500

    changed the way default config file is parsed

--- starling/data/argument_parser.py
+++ starling/data/argument_parser.py
@@ -6,34 +6,9 @@ from IPython import embed
 
 
 def get_params(config_file=None):
-    default_config = {
-        "model": {
-            "in_channels": 1,
-            "model": "ResNet18",
-            "latent_dim": 128,
-            "kernel_size": 3,
-            "loss_type": "elbo",
-            "weights_type": "reciprocal",
-            "KLD_weight": 1,
-            "lr_scheduler": "OneCycleLR",
-            "dimension": 384,
-            "base": 64,
-        },
-        "training": {
-            "project_name": "testing_elbo",
-            "learning_rate": None,
-            "num_epochs": 100,
-            "output_path": ".",
-        },
-        "data": {
-            "train_data": None,
-            "val_data": None,
-            "test_data": None,
-            "normalize": None,
-            "batch_size": 64,
-        },
-        "device": {"cuda": [0]},
-    }
+    dirname = os.path.dirname(__file__)
+    with open(f"{dirname}/default_config.yaml", "r") as stream:
+        default_config = yaml.safe_load(stream)
 
     if config_file is not None:
         with open(config_file, "r") as stream:
@@ -43,8 +18,6 @@ def get_params(config_file=None):
         config = {**default_config, **user_config}
     else:
         config = {**default_config}
-
-    os.makedirs(config["training"]["output_path"], exist_ok=True)
 
     with open(f"{config['training']['output_path']}/config.yaml", "w") as f:
         yaml.dump(config, f)
