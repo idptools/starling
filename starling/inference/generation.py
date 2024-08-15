d82c1d8d6320a1a44d23531599faf9b454c61aa6
bnovakborna <bnovak@wustl.edu>
2024-08-15 11:43:05 -0500

    sequence length agnostic generation of 3d coordinates

--- starling/inference/generation.py
+++ starling/inference/generation.py
@@ -1,16 +1,24 @@
-from starling.models.diffusion import DiffusionModel
-from starling.models.unet import UNetConditional
-from starling.models.cvae import cVAE
-from starling.structure.coordinates import compare_distance_matrices, loss_function, create_ca_topology_from_coords, save_trajectory, distance_matrix_to_3d_structure_gd
-from IPython import embed
-import torch
-import mdtraj as md
-import torch.optim as optim
-import numpy as np
-from scipy.spatial import distance_matrix
 import time
 from argparse import ArgumentParser
+
 import matplotlib.pyplot as plt
+import mdtraj as md
+import numpy as np
+import torch
+import torch.optim as optim
+from IPython import embed
+from scipy.spatial import distance_matrix
+
+from starling.models.cvae import cVAE
+from starling.models.diffusion import DiffusionModel
+from starling.models.unet import UNetConditional
+from starling.structure.coordinates import (
+    compare_distance_matrices,
+    create_ca_topology_from_coords,
+    distance_matrix_to_3d_structure_gd,
+    loss_function,
+    save_trajectory,
+)
 
 
 def plot_matrices(original, computed, difference, filename):
@@ -18,29 +26,30 @@ def plot_matrices(original, computed, difference, filename):
     fig, axs = plt.subplots(1, 3, figsize=(18, 6))
 
     # Plot the original distance matrix
-    im0 = axs[0].imshow(original, cmap='viridis')
-    axs[0].set_title('Original Distance Matrix')
-    axs[0].set_xlabel('Residue Index')
-    axs[0].set_ylabel('Residue Index')
+    im0 = axs[0].imshow(original, cmap="viridis")
+    axs[0].set_title("Original Distance Matrix")
+    axs[0].set_xlabel("Residue Index")
+    axs[0].set_ylabel("Residue Index")
     fig.colorbar(im0, ax=axs[0])
 
     # Plot the computed distance matrix
-    im1 = axs[1].imshow(computed, cmap='viridis')
-    axs[1].set_title('Computed Distance Matrix (GD)')
-    axs[1].set_xlabel('Residue Index')
-    axs[1].set_ylabel('Residue Index')
+    im1 = axs[1].imshow(computed, cmap="viridis")
+    axs[1].set_title("Computed Distance Matrix (GD)")
+    axs[1].set_xlabel("Residue Index")
+    axs[1].set_ylabel("Residue Index")
     fig.colorbar(im1, ax=axs[1])
 
     # Plot the difference matrix
-    im2 = axs[2].imshow(difference, cmap='viridis')
-    axs[2].set_title('Difference Matrix (GD)')
-    axs[2].set_xlabel('Residue Index')
-    axs[2].set_ylabel('Residue Index')
+    im2 = axs[2].imshow(difference, cmap="viridis")
+    axs[2].set_title("Difference Matrix (GD)")
+    axs[2].set_xlabel("Residue Index")
+    axs[2].set_ylabel("Residue Index")
     fig.colorbar(im2, ax=axs[2])
 
     # Save the plot to disk
     plt.savefig(filename)
     plt.close()
+
 
 def symmetrize_distance_map(dist_map):
     # Ensure the distance map is 2D
@@ -67,12 +76,13 @@ def compare_distance_matrices(original_distance_matrix, coords):
     difference_matrix = np.abs(original_distance_matrix - computed_distance_matrix)
     return computed_distance_matrix, difference_matrix
 
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("--number_maps", type=int, default=1)
-    #parser.add_argument("--sequence", type=str, default="A"*100)
-    #parser.add_argument("--sequence", type=str, default="A"*384)
-    parser.add_argument("--sequence", type=str, default="PKGS"*96)
+    # parser.add_argument("--sequence", type=str, default="A" * 200)
+    # parser.add_argument("--sequence", type=str, default="A"*384)
+    parser.add_argument("--sequence", type=str, default="PKGS" * 50)
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--steps", type=int, default=1000)
 
@@ -85,7 +95,7 @@ def main():
         norm="group",
         blocks=[2, 2, 2],
         middle_blocks=2,
-        labels_dim=384
+        labels_dim=384,
     )
 
     encoder_model = cVAE.load_from_checkpoint(
@@ -93,42 +103,64 @@ def main():
         map_location=args.device,
     )
 
-    diffusion = DiffusionModel.load_from_checkpoint('/home/bnovak/projects/test_diffusion/diffusion_testing_my_UNet_attention/model-kernel-epoch=09-epoch_val_loss=0.05.ckpt', model=UNet_model, encoder_model=encoder_model, map_location=args.device)
+    diffusion = DiffusionModel.load_from_checkpoint(
+        "/home/bnovak/projects/test_diffusion/diffusion_testing_my_UNet_attention/model-kernel-epoch=09-epoch_val_loss=0.05.ckpt",
+        model=UNet_model,
+        encoder_model=encoder_model,
+        map_location=args.device,
+    )
 
-    distance_maps, *_ = diffusion.sample(args.number_maps, labels = args.sequence, steps=args.steps)
+    distance_maps, *_ = diffusion.sample(args.number_maps, labels=args.sequence)
 
     # Initialize an empty list to store symmetrized distance maps
     sym_distance_maps = []
 
     # Iterate over each distance map
     for dist_map in distance_maps:
-        sym_dist_map = symmetrize_distance_map(dist_map)
+        sym_dist_map = symmetrize_distance_map(
+            dist_map[:, : len(args.sequence), : len(args.sequence)]
+        )
         sym_distance_maps.append(sym_dist_map)
 
     # Convert the list back to a tensor
     sym_distance_maps = torch.stack(sym_distance_maps)
-    coordinates = np.array([distance_matrix_to_3d_structure_gd(dist_map, num_iterations=10000, learning_rate=0.05, verbose=True)
-                    for dist_map in sym_distance_maps
-                ])
 
-    computed_distance_matrix_gd, difference_matrix_gd = compare_distance_matrices(sym_distance_maps[0].cpu(), coordinates[0].squeeze())
+    coordinates = np.array(
+        [
+            distance_matrix_to_3d_structure_gd(
+                dist_map, num_iterations=10000, learning_rate=0.05, verbose=True
+            )
+            for dist_map in sym_distance_maps
+        ]
+    )
+
+    computed_distance_matrix_gd, difference_matrix_gd = compare_distance_matrices(
+        sym_distance_maps[0].cpu(), coordinates[0].squeeze()
+    )
 
     original_distance_matrix = sym_distance_maps[0].cpu()
-    plot_matrices(original_distance_matrix, computed_distance_matrix_gd, difference_matrix_gd.cpu(), "distance_matrices.png")
+    plot_matrices(
+        original_distance_matrix,
+        computed_distance_matrix_gd,
+        difference_matrix_gd.cpu(),
+        "distance_matrices.png",
+    )
 
-
-    print(f"Min: {original_distance_matrix.min()}, Max: {original_distance_matrix.max()}")
+    print(
+        f"Min: {original_distance_matrix.min()}, Max: {original_distance_matrix.max()}"
+    )
     print(f"NaN values: {torch.isnan(original_distance_matrix).any()}")
     print(f"Inf values: {torch.isinf(original_distance_matrix).any()}")
-    print(f"Symmetric: {torch.allclose(original_distance_matrix, original_distance_matrix.T)}")
-
+    print(
+        f"Symmetric: {torch.allclose(original_distance_matrix, original_distance_matrix.T)}"
+    )
 
     print("\nOriginal Distance Matrix")
     print(sym_distance_maps[0].cpu())
-    
+
     print("\nComputed Distance Matrix (Gradient Descent):")
     print(computed_distance_matrix_gd)
-    
+
     print("\nDifference Matrix (Gradient Descent):")
     print(difference_matrix_gd)
 
