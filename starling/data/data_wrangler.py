5f24a9642530b9f0c2101f5f81425001b0d41b02
bnovakborna <bnovak@wustl.edu>
2024-10-07 10:10:02 -0500

    removed commented out code

--- starling/data/data_wrangler.py
+++ starling/data/data_wrangler.py
@@ -3,7 +3,6 @@ from typing import Iterator, Tuple
 import h5py
 import numpy as np
 import pandas as pd
-from IPython import embed
 
 
 def one_hot_encode(sequences):
@@ -80,27 +79,6 @@ def MaxPad(original_array: np.array, shape: tuple) -> np.array:
     )
 
 
-# def load_hdf5_compressed(file_path, frame, keys_to_load=None):
-#     """
-#     Loads data from an HDF5 file.
-
-#     Parameters:
-#         - file_path (str): Path to the HDF5 file.
-#         - keys_to_load (list): List of keys to load. If None, loads all keys.
-#     Returns:
-#         - dict: Dictionary containing loaded data.
-#     """
-#     data_dict = {}
-#     with h5py.File(file_path, "r") as f:
-#         keys = keys_to_load if keys_to_load else f.keys()
-#         for key in keys:
-#             if key == "dm":
-#                 data_dict[key] = f[key][frame]
-#             else:
-#                 data_dict[key] = f[key][...]
-#     return data_dict
-
-
 def load_hdf5_compressed(file_path, frame=None, keys_to_load=None):
     """
     Loads data from an HDF5 file, optionally for a specific frame.
@@ -122,71 +100,6 @@ def load_hdf5_compressed(file_path, frame=None, keys_to_load=None):
             else:
                 data[key] = f[key][...]
     return data
-
-
-# def load_hdf5_compressed(file_path, frame=None, keys_to_load=None):
-#     """
-#     Loads data from an HDF5 file.
-
-#     Parameters:
-#         - file_path (str): Path to the HDF5 file.
-#         - frame (int): Frame index to load if applicable. If None, loads all frames.
-#         - keys_to_load (list): List of keys to load. If None, loads all keys.
-
-#     Returns:
-#         - dict: Dictionary containing loaded data.
-#     """
-#     data = {}
-#     with h5py.File(file_path, "r") as f:
-#         keys = keys_to_load if keys_to_load else f.keys()
-#         for key in keys:
-#             if isinstance(f[key], h5py.Dataset):
-#                 if len(f[key].shape) > 0:
-#                     if key == "dm" and frame is not None:
-#                         # Index the "dm" key with the specified frame
-#                         data[key] = f[key][frame]
-#                     elif key == "seq":
-#                         # Index the "seq" key at index 0
-#                         data[key] = f[key][0]
-#                     else:
-#                         # Load the entire dataset for other keys
-#                         data[key] = f[key][...]
-#                 else:
-#                     # Handle datasets with no dimensions (scalar or 1D)
-#                     data[key] = f[key][...]
-                
-#                 # Handle string conversion if needed
-#                 if f[key].dtype.kind == "S":  # Check if the data type is a string
-#                     data[key] = data[key].astype(str)  # Convert bytes to string
-#             else:
-#                 # Load non-dataset objects if they exist
-#                 data[key] = f[key][...]
-    
-#     return data
-
-
-# def read_tsv_file(tsv_file: str) -> List:
-#     """
-#     A function that reads the paths to distance maps from a txt file
-
-#     Parameters
-#     ----------
-#     txt_file : str
-#         A path to a tsv file containing the paths to distance maps as a first column
-#         and index of a distance map to load as a second column
-
-#     Returns
-#     -------
-#     List
-#         A list of paths to distance maps
-#     """
-#     paths = []
-#     with open(tsv_file, "r") as file:
-#         for line in file:
-#             line = line.strip()
-#             line = line.split("\t")[0:2]
-#             paths.append(line)
-#     return paths
 
 
 def read_tsv_file(tsv_file: str) -> Iterator[Tuple[str, str]]:
