17d82e1aeba1fd8087eefbee04808e55c45a025d
bnovakborna <bnovak@wustl.edu>
2025-06-26 13:40:26 -0500

    testing KLD warmup

--- starling/models/vae.py
+++ starling/models/vae.py
@@ -702,9 +702,9 @@ class VAE(pl.LightningModule):
 
     def on_train_start(self):
         # Calculate correct training steps (not including validation)
-        # steps_per_epoch = len(self.trainer.train_dataloader) // self.trainer.num_devices
-        # training_steps = steps_per_epoch * self.trainer.max_epochs
-        self.kld_scheduler.configure(self.trainer.estimated_stepping_batches)
+        steps_per_epoch = len(self.trainer.train_dataloader)
+        total_training_steps = steps_per_epoch * self.trainer.max_epochs
+        self.kld_scheduler.configure(total_training_steps)
 
     def _reset_epoch_metrics(self) -> None:
         """Reset all epoch-level metric accumulators to zero."""
